213adf5a514daef713bb15aad72bb9b29243f35d
Soundarya <31506228+soundaryavsb@users.noreply.github.com>
2025-03-20 22:23:51 +0530

    Add files via upload
    Part 01 Data Driven Hard code

---
+++ Part01_Class01CorrectUserName.java
@@ -0,0 +1,38 @@
+package part01_Introduction_LoginTestCases;
+
+import java.util.concurrent.ConcurrentHashMap.KeySetView;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Part01_Class01CorrectUserName {
+
+	@Test
+	public void loginWithCorrectUserName() throws InterruptedException
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\chrome Driver\\chromedriver-win32-v134\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		driver.manage().window().maximize();
+		
+		Thread.sleep(3000);
+		
+		WebElement UserName=driver.findElement(By.name("username"));
+//		UserName.click();
+		UserName.sendKeys("Admin");
+		
+		WebElement Password=driver.findElement(By.xpath("//*[@name='password']"));
+		Password.sendKeys("admin");
+		
+		Thread.sleep(2000);
+		
+		WebElement Login=driver.findElement(By.xpath("//*[@type='submit']"));
+		Login.click();
+		
+		driver.quit();
+	}
+}
