af3b6e206776ebe93b3bef7fa41c8b764c8d1b5f
Soundarya <31506228+soundaryavsb@users.noreply.github.com>
2025-03-20 22:30:00 +0530

    Add files via upload
    Part02_Parameter login

---
+++ Part02_Class04BothIncorrect.java
@@ -0,0 +1,36 @@
+package part02_Parameters_LoginTestCases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class Part02_Class04BothIncorrect {
+	@Test
+	@Parameters({"UserName","PassWord"})
+	public void loginWithBothIncorrect(String UserName_Data,String PassWord_Data) throws InterruptedException
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\chrome Driver\\chromedriver-win32-v134\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		driver.manage().window().maximize();
+		
+		Thread.sleep(3000);
+		
+		WebElement UserName=driver.findElement(By.name("username"));
+//		UserName.click();
+		UserName.sendKeys(UserName_Data);
+		
+		WebElement Password=driver.findElement(By.xpath("//*[@name='password']"));
+		Password.sendKeys(PassWord_Data);
+		
+		Thread.sleep(2000);
+		
+		WebElement Login=driver.findElement(By.xpath("//*[@type='submit']"));
+		Login.click();
+		
+		driver.quit();
+	}
+}
