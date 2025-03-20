01ebf267f40991568d932ba55ab98e1f27276b54
Soundarya <31506228+soundaryavsb@users.noreply.github.com>
2025-03-20 22:10:36 +0530

    Add files via upload
    Part 01 frameWork

---
+++ Class02CorrectPassword.java
@@ -0,0 +1,34 @@
+package part01_Introduction_LoginTestCases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Class02CorrectPassword {
+	@Test
+	public void loginWithCorrectPassword() throws InterruptedException
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\chrome Driver\\chromedriver-win32-v134\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		driver.manage().window().maximize();
+		
+		Thread.sleep(3000);
+		
+		WebElement UserName=driver.findElement(By.name("username"));
+//		UserName.click();
+		UserName.sendKeys("Admin1");
+		
+		WebElement Password=driver.findElement(By.xpath("//*[@name='password']"));
+		Password.sendKeys("admin123");
+		
+		Thread.sleep(2000);
+		
+		WebElement Login=driver.findElement(By.xpath("//*[@type='submit']"));
+		Login.click();
+		
+		driver.quit();
+	}
+}
